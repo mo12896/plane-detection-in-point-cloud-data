3e1a13ad09c32d7c51f9e23c2f848265b2d7c7df
mo12896 <moritz2018.mueller@outlook.de>
2022-07-27 17:15:21 +0200

    Modify the setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # pull image from docker hub
-docker pull ransac
+docker pull mo12896/ransac
 # run the image using bind mounts to the data directory
 docker run -v /home/moritz/PycharmProjects/plane-detection-in-point-cloud-data/data:/home/data ransac
