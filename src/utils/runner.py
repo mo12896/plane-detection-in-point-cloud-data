4fd37611096bf9a67f14970e50a1160323f014e5
@moritz2018.mueller <moritz2018.mueller@outlook.de>
2022-10-12 17:08:55 +0200

    Modify runner file

--- src/utils/runner.py
+++ src/utils/runner.py
@@ -34,13 +34,16 @@ class Runner:
         Args:
             file (Any): file in raw directory
         """
-
-        filename = os.fsdecode(file)
-        if filename.endswith(tuple([enum.name.lower() for enum in self.pc_formats])):
-            cloud = self.plane_detector.detect_planes(filename)
-            # self.plane_detector.store_best_eqs(filename)
-            if self.configs["VERBOSE"]:
-                self.plane_detector.display_pointcloud(cloud)
+        try:
+            filename = os.fsdecode(file)
+            if filename.endswith(
+                tuple([enum.name.lower() for enum in self.pc_formats])
+            ):
+                cloud = self.plane_detector.detect_planes(filename)
+                if self.configs["VERBOSE"]:
+                    self.plane_detector.display_pointcloud(cloud)
+        except Exception as exc:
+            print(exc)
 
     def remove_plane(self, file: Any):
         """Remove planes from a single point cloud
@@ -48,13 +51,18 @@ class Runner:
         Args:
             file (Any): file in raw directory
         """
-        filename = os.fsdecode(file)
-        if filename.endswith(tuple([enum.name.lower() for enum in self.pc_formats])):
-            cloud = self.plane_remover.remove_planes(filename)
+        try:
+            filename = os.fsdecode(file)
+            if filename.endswith(
+                tuple([enum.name.lower() for enum in self.pc_formats])
+            ):
+                cloud = self.plane_remover.remove_planes(filename)
 
-            if self.configs["OUT_REMOVAL"]["USE"]:
-                if self.configs["VERBOSE"]:
+                if self.configs["OUT_REMOVAL"]["USE"]:
+                    if self.configs["VERBOSE"]:
+                        self.plane_remover.display_pointcloud(cloud)
+                    self.out_remover.run(filename)
+                else:
                     self.plane_remover.display_pointcloud(cloud)
-                self.out_remover.run(filename)
-            else:
-                self.plane_remover.display_pointcloud(cloud)
+        except Exception as exc:
+            print(exc)
